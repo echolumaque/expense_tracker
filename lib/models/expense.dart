9a52f3b8ab3eb1d434cbb59322857360e0697d75
Jhericoh Janquill Lumaque <ljhericoh15@gmail.com>
2024-05-29 04:14:38 +0800

    Started developing expenses list

---
+++ lib/models/expense.dart
@@ -0,0 +1,19 @@
+import 'package:uuid/uuid.dart';
+import '../enums/category.dart';
+
+const uuid = Uuid();
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+}
